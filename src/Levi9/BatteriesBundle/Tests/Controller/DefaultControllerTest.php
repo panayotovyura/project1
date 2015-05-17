e76a65da7c65b78451c7c931ee9f69cddba2341d
Yura Panayotov <panayotov.yura@gmail.com>
2015-05-17 22:06:13 +0300

    add test

--- src/Levi9/BatteriesBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Levi9/BatteriesBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,55 @@
 namespace Levi9\BatteriesBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * @dataProvider addProvider
+     */
+    public function testAdd($type, $count)
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request(Request::METHOD_GET, '/add');
+        $form = $crawler->selectButton('levi9_batteriesbundle_batteries[save]')->form();
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $form['levi9_batteriesbundle_batteries[type]'] = $type;
+        $form['levi9_batteriesbundle_batteries[count]'] = $count;
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect('/'));
+    }
+
+    public function addProvider()
+    {
+        return [
+            ['AA', 4],
+            ['AAA', 3],
+            ['AA', 1],
+        ];
+    }
+
+    public function testStatistics()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(Request::METHOD_GET, '/');
+
+        $this->assertEquals(
+            $crawler->filter('tbody tr')->count(),
+            2
+        );
+
+        $this->assertEquals(
+            $crawler->filter('td:contains("AA")')->siblings()->text(),
+            5
+        );
+
+        $this->assertEquals(
+            $crawler->filter('td:contains("AAA")')->siblings()->text(),
+            3
+        );
     }
 }
